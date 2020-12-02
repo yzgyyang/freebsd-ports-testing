33bcd7267cd0789320b5e1a973e5f526b3c017fc
Guangyuan Yang <yzgyyang@outlook.com>
2020-12-02 15:05:07 +0800

    Specify poudriere tree for bulk actions

--- test_script.sh
+++ test_script.sh
@@ -8,7 +8,7 @@ PATCH_NAME="${PORT_CAT}.${PORT_NAME}.${PORT_VER}.diff"
 cd ${PD_TREE_PATH} && git apply ${CIRRUS_WORKING_DIR}/patches/${PATCH_NAME}
 
 # obtained from: https://github.com/freebsd/freebsd-ports-libreoffice/blob/8a09dffcc6a1502987ae054e43ac6c61c806d267/porttest.sh
-poudriere bulk -t -j ${PD_JAIL} net/nc
+poudriere bulk -t -j ${PD_JAIL} -p ${PD_TREE} net/nc
 cd ${PD_TREE_PATH}/${CIRRUS_BRANCH}
 pkg fetch -y -o pkgs `make missing-packages`
 rm -rf /usr/local/poudriere/data/packages/jail-default/.latest/All
