b9a23df0e2a6c22ac77565039920bd06528917d6
Guangyuan Yang <yzgyyang@outlook.com>
2020-12-02 14:44:58 +0800

    Obtained pkg cache from freebsd/freebsd-ports-libreoffice

--- install_script.sh
+++ install_script.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -e
 
-pkg update
-pkg upgrade -y
+sed -i.bak -e 's,pkg+http://pkg.FreeBSD.org/\${ABI}/quarterly,pkg+http://pkg.FreeBSD.org/\${ABI}/latest,' /etc/pkg/FreeBSD.conf
+pkg update -f
 pkg install -y git poudriere
-git clone --depth 1 --single-branch -b master https://github.com/freebsd/freebsd-ports /opt/freebsd-ports
+git clone --depth 1 --single-branch -b master https://github.com/freebsd/freebsd-ports ${PD_TREE_PATH}
 
 echo "NO_ZFS=yes" >> /usr/local/etc/poudriere.conf
 sed -i.bak -e 's,FREEBSD_HOST=_PROTO_://_CHANGE_THIS_,FREEBSD_HOST=https://download.FreeBSD.org,' /usr/local/etc/poudriere.conf
+mkdir -p /usr/local/poudriere
 
 poudriere jail -c -j ${PD_JAIL} -v ${PD_JAIL_RELEASE} -a ${PD_ARCH}
 poudriere ports -c -m null -M ${PD_TREE_PATH} -p ${PD_TREE}
--- test_script.sh
+++ test_script.sh
@@ -4,5 +4,15 @@ PORT_CAT=$(echo ${CIRRUS_BRANCH} | awk -F '/' '{print $1}')
 PORT_NAME=$(echo ${CIRRUS_BRANCH} | awk -F '/' '{print $2}')
 PORT_VER=$(echo ${CIRRUS_CHANGE_TITLE} | awk -F ':' '{print $2}')
 PATCH_NAME="${PORT_CAT}.${PORT_NAME}.${PORT_VER}.diff"
+
 cd ${PD_TREE_PATH} && git apply ${CIRRUS_WORKING_DIR}/patches/${PATCH_NAME}
+
+# obtained from: https://github.com/freebsd/freebsd-ports-libreoffice/blob/8a09dffcc6a1502987ae054e43ac6c61c806d267/porttest.sh
+poudriere bulk -t -j ${PD_JAIL} net/nc
+cd ${PD_TREE_PATH}/${CIRRUS_BRANCH}
+pkg fetch -y -o pkgs `make missing-packages`
+rm -rf /usr/local/poudriere/data/packages/jail-default/.latest/All
+mv pkgs/All /usr/local/poudriere/data/packages/jail-default/.latest/
+rm -rf pkgs
+
 poudriere testport -j ${PD_JAIL} -p ${PD_TREE} -o ${CIRRUS_BRANCH}
