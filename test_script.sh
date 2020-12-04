c1835158523d3022b78c6f9a7f4a473e624a0ca8
Guangyuan Yang <yzgyyang@outlook.com>
2020-12-04 18:00:33 +0800

    Abandon pkg cache for now

--- test_script.sh
+++ test_script.sh
@@ -9,14 +9,4 @@ PD_ABI="FreeBSD:${PD_JAIL_RELEASE_A}:${PD_ARCH}"
 
 cd ${PD_TREE_PATH} && git apply ${CIRRUS_WORKING_DIR}/patches/${PATCH_NAME}
 
-# obtained from: https://github.com/freebsd/freebsd-ports-libreoffice/blob/8a09dffcc6a1502987ae054e43ac6c61c806d267/porttest.sh
-poudriere bulk -t -j ${PD_JAIL} -p ${PD_TREE} net/nc
-cd ${PD_TREE_PATH}/${CIRRUS_BRANCH}
-sed -i.bak -e "s,pkg+http://pkg.FreeBSD.org/\${ABI}/quarterly,pkg+http://pkg.FreeBSD.org/${PD_ABI}/latest," /etc/pkg/FreeBSD.conf
-pkg update -f
-pkg fetch -y -o pkgs `make missing-packages`
-rm -rf /usr/local/poudriere/data/packages/${PD_JAIL}-${PD_TREE}/.latest/All
-mv pkgs/All /usr/local/poudriere/data/packages/${PD_JAIL}-${PD_TREE}/.latest/
-rm -rf pkgs
-
 poudriere testport -j ${PD_JAIL} -p ${PD_TREE} -o ${CIRRUS_BRANCH}
